3e40a9f4f0798ffc350787e2c546e4cbe1866d59
yeonggwangchoi <dudrhkd7811@naver.com>
2025-07-20 03:40:25 +0900

    Update src

--- src/launch_pkg/launch/main.launch.py
+++ src/launch_pkg/launch/main.launch.py
@@ -21,6 +21,30 @@ def generate_launch_description():
             name='lane_info_extractor_node',
             output='screen'
         ),
+        # Node(
+        #     package='camera_perception_pkg',
+        #     executable='traffic_light_detector_node',
+        #     name='traffic_light_detector_node',
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='lidar_perception_pkg',
+        #     executable='lidar_publisher_node',
+        #     name='lidar_publisher_node',
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='lidar_perception_pkg',
+        #     executable='lidar_processor_node',
+        #     name='lidar_processor_node',
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='lidar_perception_pkg',
+        #     executable='lidar_obstacle_detector_node',
+        #     name='lidar_obstacle_detector_node',
+        #     output='screen'
+        # ),
         Node(
             package='decision_making_pkg',
             executable='motion_planner_node',
